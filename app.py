96a47e2dde6ee813297cc9b84c8ea3dbfd77037e
Hemant0022 <hemantraja85@gmail.com>
2026-07-31 20:34:51 +0530

    Updated Commit

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ load_dotenv()
 import streamlit as st
 
 from modules import home_page, resume_page, interview_page, roadmap_page, recruiter_dashboard
-os.environ["HF_TOKEN"] = st.secrets.get("HF_TOKEN")
+# os.environ["HF_TOKEN"] = st.secrets.get("HF_TOKEN")
 st.set_page_config(
     page_title="AI Hiring Intelligence",
     page_icon="🧠",
